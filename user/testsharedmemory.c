18f3d63c77187d89b7d357ac099c8e79a8ff10fe
jjaimez <jaimezjacinto@gmail.com>
2015-08-17 14:53:55 -0300

    readme y testshmmem con 2 paginas

--- user/testsharedmemory.c
+++ user/testsharedmemory.c
@@ -39,7 +39,7 @@ int semaphore,pid,n,keyIndex, keyArray;
   shm_get(keyIndex,&index); //get a shared memory 
   *index = (int)0; // init with 0 the shared memory
 
-  keyArray = shm_create(4000); //shared memory with two pages 
+  keyArray = shm_create(5000); //shared memory with two pages 
   char* array= 0;
   shm_get(keyArray,&array); 
 
@@ -65,16 +65,16 @@ int semaphore,pid,n,keyIndex, keyArray;
   for (n=0; n<NUMBERS*N;  n ++){
     printf(1,"n= %d  array= %d  \n", n , *(array+n) );
   }
-  printf(1,"%s\n","load the array with 4096 numbers (two pages)" );
-  for (n=0; n<4096; n ++){
+  printf(1,"%s\n","load the array with 8192 numbers (two pages)" );
+  for (n=0; n<8192; n ++){
     *(array+n) = 4;
   }
   printf(1,"%s\n", "reading the 6 last numbers");
-  for (n=4090; n<4096; n ++){
+  for (n=8186; n<8192; n ++){
     printf(1,"array= %d  \n",*(array+n) ); 
   }
   printf(1,"%s\n", "reading the 6 last numbers, catch the trap in the 7 number ");
-  for (n=4090; n<4097; n ++){
+  for (n=8186; n<8193; n ++){
     printf(1,"array= %d  \n",*(array+n) ); 
   }
   semfree(semaphore);
